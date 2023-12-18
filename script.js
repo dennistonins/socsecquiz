ac5946d993c1bc7fd7f2b90f5c98a278beff20cd
dennistonins <50176337+dennistonins@users.noreply.github.com>
2023-12-18 14:36:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ let selectedModule = null;
 
 // Dummy CSV data loader (replace with your CSV loading logic)
 async function loadQuestionsFromCSV() {
-    // Replace this with actual CSV loading logic
-    return [
-        { module: 'module1', question: 'Question 1', answers: ['A', 'B', 'C', 'D'], correctAnswer: 0 },
-        // ... other questions ...
-    ];
+    const response = await fetch('questions.csv');
+    const csvText = await response.text();
+    return csvText.split('\n').slice(1).map(line => {
+        const [module, question, a1, a2, a3, a4, correctAnswerIndex] = line.split(',');
+        return {
+            module: module.trim(),
+            question: question.trim(),
+            answers: [a1, a2, a3, a4].map(answer => answer.trim()),
+            correctAnswer: parseInt(correctAnswerIndex.trim())
+        };
+    });
 }
 
 // Load questions from CSV

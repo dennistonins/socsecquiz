b0d0fa7d80c92c6f8406fb1e44a792fe1f8a2187
dennistonins <50176337+dennistonins@users.noreply.github.com>
2023-12-18 14:03:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ let correctAnswerRevealed = false;
 let selectedModule = null;
 
 // CSV loading functions (unchanged)
-
 async function loadQuestionsFromCSV(file) {
   const csvData = await loadCSV(file);
   return parseCSV(csvData);
 }
 
 const questionsPromise = loadQuestionsFromCSV('questions.csv');
-
 questionsPromise.then((loadedQuestions) => {
   questions.push(...loadedQuestions);
   currentQuestionIndex = Math.floor(Math.random() * questions.length);
@@ -23,7 +21,6 @@ questionsPromise.then((loadedQuestions) => {
 });
 
 // Helper functions (unchanged)
-
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -32,15 +29,14 @@ function shuffleArray(array) {
 }
 
 // Flashcard functions (unchanged)
-
 function loadHomeScreen() {
   document.getElementById('homeScreen').style.display = 'flex';
   document.getElementById('flashcard').style.display = 'none';
 }
 
 function showFlashcardView() {
-    document.getElementById('homeScreen').style.display = 'none'; // Hide home screen
-    document.getElementById('flashcard').style.display = 'flex'; // Show flashcard view
+  document.getElementById('homeScreen').style.display = 'none';
+  document.getElementById('flashcard').style.display = 'flex';
 }
 
 function loadQuestion() {
@@ -52,14 +48,33 @@ function loadQuestion() {
 
   console.log('Filtered questions:', filteredQuestions);
 
-  // ... existing loadQuestion logic ...
+  const currentQuestion = filteredQuestions[currentQuestionIndex];
+  const questionDiv = document.getElementById('question');
+  const answersDiv = document.getElementById('answers');
+
+  // Clear existing content
+  questionDiv.innerText = '';
+  answersDiv.innerHTML = '';
+
+  // Set question text
+  questionDiv.innerText = currentQuestion.question;
+
+  // Shuffle and display answer options
+  const shuffledAnswers = shuffleArray([...currentQuestion.answers]);
+  shuffledAnswers.forEach((answer, index) => {
+    const answerButton = document.createElement('button');
+    answerButton.innerText = answer;
+    answerButton.classList.add('answer');
+    answerButton.onclick = () => selectAnswer(index);
+    answersDiv.appendChild(answerButton);
+  });
 }
 
 function selectModule(module) {
-    console.log('Selected module:', module);
-    selectedModule = module;
-    showFlashcardView(); // Add this line
-    loadQuestion();
+  console.log('Selected module:', module);
+  selectedModule = module;
+  showFlashcardView();
+  loadQuestion();
 }
 
 function selectAnswer(selectedIndex) {
@@ -68,38 +83,12 @@ function selectAnswer(selectedIndex) {
 }
 
 function checkAnswer() {
-  const currentQuestion = questions[currentQuestionIndex];
-  const correctAnswerIndex = currentQuestion.shuffledCorrectAnswer;
-
-  const answers = document.querySelectorAll('.answer');
-  answers.forEach((answer, index) => {
-    answer.classList.remove('correct', 'wrong', 'selected');
-
-    if (index === correctAnswerIndex && selectedAnswerIndex === correctAnswerIndex) {
-      answer.classList.add('correct', 'selected');
-    } else if (index === selectedAnswerIndex && index !== correctAnswerIndex) {
-      answer.classList.add('wrong');
-    }
-  });
-
-  correctAnswerRevealed = true;
+  // ... existing checkAnswer logic ...
 }
 
 function nextQuestion() {
-  selectedAnswerIndex = null;
-  correctAnswerRevealed = false;
-
-  const currentQuestion = questions[currentQuestionIndex];
-  currentQuestion.shuffledAnswers = null;
-  currentQuestion.shuffledCorrectAnswer = null;
-
-  currentQuestionIndex = Math.floor(Math.random() * questions.length);
-
-  loadQuestion();
+  // ... existing nextQuestion logic ...
 }
 
 // Initial load (unchanged)
-
 loadQuestion();
-
-

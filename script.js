94f4aab07793ebb761a01c367e79dad6708b8277
dennistonins <50176337+dennistonins@users.noreply.github.com>
2023-12-17 23:04:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const questions = [
-  { question: "What is the total percentage of an employee's earned income (up to the annual limit) that is paid each year into Social Security by the employee and employer?", answers: ["0.062", "0.029", "0.153", "0.124"], correctAnswer: 1 },
+  { question: "What is the total percentage of an employee's earned income (up to the annual limit) that is paid each year into Social Security by the employee and employer?", answers: ["0.062", "0.029", "0.153", "0.124"], correctAnswer: 0 },
+  { question: "If a retiree with a FRA of 67 collects retirement benefits at age 66, what percentage of their PIA will they receive?", answers: ["1.08", "0.86", "0.93", "1"], correctAnswer: 0 },
+
   // Add more questions here...
 ];
 let currentQuestionIndex = 0;

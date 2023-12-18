6d9f7f175f25a79cff517f757f987e8b4202a947
dennistonins <50176337+dennistonins@users.noreply.github.com>
2023-12-18 02:06:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-const csvFilePath = '/questions.csv'; // Adjust the file path accordingly
+// CSV parsing function
+function parseCSV(csv) {
+  const lines = csv.split('\n');
+  const headers = lines[0].split(',');
 
-let questions = [];
-let currentQuestionIndex = null;
-let selectedAnswerIndex = null;
-let correctAnswerRevealed = false;
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+  return lines.slice(1).map(line => {
+    const values = line.split(',');
+    return headers.reduce((obj, header, index) => {
+      obj[header.trim()] = values[index].trim();
+      return obj;
+    }, {});
+  });
 }
 
+// Function to load questions from CSV
+function loadQuestionsFromCSV() {
+  fetch('questions.csv') // Update with your actual CSV file path
+    .then(response => response.text())
+    .then(data => {
+      questions = parseCSV(data);
+      console.log('Parsed Questions:', questions);
+      currentQuestionIndex = Math.floor(Math.random() * questions.length);
+      loadQuestion();
+    })
+    .catch(error => console.error('Error loading CSV:', error));
+}
+
+// Load question function
 function loadQuestion() {
   const currentQuestion = questions[currentQuestionIndex];
   document.getElementById('question').innerText = currentQuestion.question;
@@ -35,59 +49,7 @@ function loadQuestion() {
   document.getElementById('answers').innerHTML = answersHtml;
 }
 
-function parseCSV(csv) {
-  const lines = csv.split('\n');
-  const headers = lines[0].split(',');
-  const result = [];
-
-  for (let i = 1; i < lines.length; i++) {
-    const obj = {};
-    const currentLine = lines[i].split(',');
-
-    for (let j = 0; j < headers.length; j++) {
-      obj[headers[j].trim()] = currentLine[j].trim();
-    }
-
-    result.push(obj);
-  }
-
-  return result;
-}
-
-function loadQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  document.getElementById('question').innerText = currentQuestion.question;
-
-  // Copy the answers array and shuffle the order only if it's a new question
-  const shuffledAnswers = currentQuestion.shuffledAnswers || [
-    currentQuestion.answer1,
-    currentQuestion.answer2,
-    currentQuestion.answer3,
-    currentQuestion.answer4
-  ];
-
-  if (!currentQuestion.shuffledAnswers) {
-    shuffleArray(shuffledAnswers);
-
-    // Update the correct answer index for the shuffled answers
-    currentQuestion.shuffledCorrectAnswer = shuffledAnswers.indexOf(currentQuestion['correctAnswer']);
-
-    currentQuestion.shuffledAnswers = shuffledAnswers;
-  }
-
-  const answersHtml = shuffledAnswers.map((answer, index) => {
-    const selectedClass = index === selectedAnswerIndex ? 'selected' : '';
-    return `<div class="answer ${selectedClass}" onclick="selectAnswer(${index})">${answer}</div>`;
-  }).join('');
-
-  document.getElementById('answers').innerHTML = answersHtml;
-}
-
-function selectAnswer(selectedIndex) {
-  selectedAnswerIndex = selectedIndex;
-  loadQuestion();
-}
-
+// Check answer function
 function checkAnswer() {
   const currentQuestion = questions[currentQuestionIndex];
   const correctAnswerIndex = parseInt(currentQuestion.shuffledCorrectAnswer);
@@ -95,7 +57,7 @@ function checkAnswer() {
   const answers = document.querySelectorAll('.answer');
   answers.forEach((answer, index) => {
     answer.classList.remove('correct', 'wrong', 'selected'); // Clear previous answer highlighting
-    if (index === correctAnswerIndex && selectedAnswerIndex === correctAnswerIndex) {
+    if (index === correctAnswerIndex && index === selectedAnswerIndex) {
       answer.classList.add('correct', 'selected'); // Green for correct answer if selected
     } else if (index === selectedAnswerIndex && index !== correctAnswerIndex) {
       answer.classList.add('wrong'); // Red for wrong answer
@@ -105,6 +67,7 @@ function checkAnswer() {
   correctAnswerRevealed = true;
 }
 
+// Next question function
 function nextQuestion() {
   selectedAnswerIndex = null;
   correctAnswerRevealed = false;
@@ -120,5 +83,5 @@ function nextQuestion() {
   loadQuestion();
 }
 
-// Initial load
+// Initial load from CSV
 loadQuestionsFromCSV();

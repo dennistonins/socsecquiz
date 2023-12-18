148308dfed9e1bca6be1eeea7a865f6346ca78c4
dennistonins <50176337+dennistonins@users.noreply.github.com>
2023-12-17 23:24:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -39,21 +39,23 @@ const questions = [
 
   // Add more questions here...
 ];
-let currentQuestionIndex = 0;
+let selectedAnswerIndex = null; // Track the user's selected answer
 
 function loadQuestion() {
   const currentQuestion = questions[currentQuestionIndex];
   document.getElementById('question').innerText = currentQuestion.question;
 
   const answersHtml = currentQuestion.answers.map((answer, index) => {
-    return `<div class="answer" onclick="selectAnswer(${index})">${answer}</div>`;
+    const selectedClass = index === selectedAnswerIndex ? 'selected' : '';
+    return `<div class="answer ${selectedClass}" onclick="selectAnswer(${index})">${answer}</div>`;
   }).join('');
 
   document.getElementById('answers').innerHTML = answersHtml;
 }
 
 function selectAnswer(selectedIndex) {
-  // Handle user selection (if needed)
+  selectedAnswerIndex = selectedIndex;
+  loadQuestion();
 }
 
 function checkAnswer() {
@@ -64,21 +66,17 @@ function checkAnswer() {
   answers.forEach((answer, index) => {
     if (index === correctAnswerIndex) {
       answer.classList.add('correct');
+    } else {
+      answer.classList.remove('correct');
     }
   });
 }
 
 function nextQuestion() {
-  // Reset highlighting
-  const answers = document.querySelectorAll('.answer');
-  answers.forEach(answer => {
-    answer.classList.remove('correct');
-  });
-
+  selectedAnswerIndex = null; // Reset the selected answer when moving to the next question
   currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
   loadQuestion();
 }
 
 // Initial load
 loadQuestion();
-
